9fa4b719641a2351356785a6f91a7e079f36a4b7
123quatrecinq <96394817+AmielCo@users.noreply.github.com>
2022-04-18 18:00:04 +0300

    Connecting to db (db empty)

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import "dotenv/config";
+import knex from 'knex';
 import cors from "cors";
 import bodyParser from "body-parser";
 import logRoute from "./middlewares/logRoute.js";
+/* import authRoutes from "./routes/authRoutes.js";
+ */import knexConfig from "./data/knexfile.js";
 
+const gameappDb = knex(knexConfig);
 
 const app = express();
 
 app.use(logRoute);
 app.use(express.json());
 app.use(cors());
+
+
+/* app.use("/auth", authRoutes);
+ */
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
