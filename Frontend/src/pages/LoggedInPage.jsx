c51d019803b49c72dc0b10135f1d1ce8424dba16
123quatrecinq <96394817+AmielCo@users.noreply.github.com>
2022-04-21 15:42:19 +0300

    rules of the game

--- Frontend/src/pages/LoggedInPage.jsx
+++ Frontend/src/pages/LoggedInPage.jsx
@@ -16,6 +16,31 @@ const LoggedInPage = () => {
       <h2>Last Score: {recentScore}</h2>
       <h2>High Score: {highScore}</h2>
 
+      <p>
+        Memory has long been a favorite game for all generations. It is easy to
+        play, in fact it is so simple that really young children can play with
+        ease. It requires observation, concentration and a good memory to win.{" "}
+        The game is also known as Concentration, Pelmanism, Shinkei-suijaku,
+        Pexeso and Pairs.
+      </p>
+
+      <p>
+        The goal of the game is to find all the pairs of cards. The cards are
+        shuffled and then each card is shown to the player. The player then
+        clicks on a card and the card is flipped. If the card is a match, the
+        player can click on another card. If the cards are a match, the cards
+        remain flipped. If the cards are not a match, the cards are flipped back
+        over. The player must try to find all the pairs of cards.
+      </p>
+
+      <p>The best score possible is 6 turns.</p>
+
+      <p>
+        {" "}
+        Good luck and have as much fun as you can with this wonderful wonderful
+        game.
+      </p>
+
       <button className="btn">
         {" "}
         <NavLink to="/Game">Start Playing</NavLink>
